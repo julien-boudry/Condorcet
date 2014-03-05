414fd26f8cb434d04076041c01dfd9152a13bbfb
Julien Boudry <julien.boudry@supinternet.fr>
2014-03-05 22:28:10 +0100

    add_vote method & improvements

--- Condorcet.class.php
+++ Condorcet.class.php
@@ -11,17 +11,19 @@ class Condorcet
 	protected $_votes ;
 
 	// Mécanique
-	protected $_i_option_id = 'A' ;
-
+	protected $_i_option_id	= 'A' ;
+	protected $_vote_state	= FALSE ;
+	protected $_options_count = 0 ;
 
 
 	// Constructor
 
 	public function __construct ($method = 'Schulze')
 	{
-		$this->_method = $method ;
+		$this->_method 	= $method ;
 
-		$this->_options = array() ;
+		$this->_options	= array() ;
+		$this->_votes 	= array() ;
 	}
 
 	public function set_method ($method)
@@ -34,10 +36,11 @@ class Condorcet
 
 	public function add_option ($option_id = null)
 	{
+		// only if the vote has not started
+		if ( $this->_vote_state === TRUE ) { return "error : Les votes ont commencé" ; }
 		
-		// Filtrage
+		// Filter
 		if ( !is_null($option_id) && !is_string($option_id) && !is_int($option_id) )
-
 			{ return "error : Mauvaise saisie de l'option" ; }
 
 		
@@ -51,8 +54,9 @@ class Condorcet
 			}
 
 			$this->_options[] = $this->_i_option_id ;
+			$this->_options_count++ ;
 
-			return TRUE ;
+			return $this->_i_option_id ;
 
 		}
 		else // Try to add the option_id
@@ -61,6 +65,7 @@ class Condorcet
 			if ( $this->try_add_option($option_id) )
 			{
 				$this->_options[] = $option_id ;
+				$this->_options_count++ ;
 
 				return TRUE ;
 			}
@@ -75,12 +80,111 @@ class Condorcet
 
 			protected function try_add_option ($option_id)
 			{
-				return !in_array($option_id, $this->_options) ;
+				return !$this->option_id_exist($option_id) ;
 			}
 
 
 
+	public function count_options ()
+	{
+		return $this->_options_count ;
+	}
+
+
+
+
 	// Register votes
+
+	public function close_options_config ()
+	{
+		$this->_vote_state = TRUE ;
+	}
+
+	public function add_vote (array $vote)
+	{
+
+		// Close vote if needed
+		if ( $this->_vote_state === FALSE ) { $this->close_options_config(); }
+
+
+		// Check array format
+		if ( $this->check_vote_input($vote) === FALSE ) 
+			{ return "error : Input de vote mal formulé"; }
+
+		// Sort
+		ksort($vote);
+
+		// Register vote
+		$this->register_vote($vote) ;
+
+
+		return TRUE ;
+	}
+
+			protected function check_vote_input ($vote)
+			{
+
+				$list_option = array() ;
+
+				if ( isset($vote[0]) || count($vote) > $this->_options_count || count($vote) < 1 )
+					{ return FALSE ; }
+
+				foreach ($vote as $key => $value)
+				{
+
+					// Check key
+					if ( !is_numeric($key) || $key > $this->_options_count )
+						{ return FALSE ; }
+
+
+					// Check options
+					if ( empty($value) )
+						{ return FALSE ; }
+
+					$options = explode(',', $value) ;
+
+
+					foreach ($options as $option)
+					{
+
+						if ( !$this->option_id_exist($option) )
+						{
+							return FALSE ;
+						}
+
+						// Don't do 2x the same option
+						if ( !in_array($option, $list_option)  ) { $list_option[] = $option ; }
+							else { return FALSE ; }
+
+
+					}
+
+				}
+
+				return TRUE ;
+			}
+
+			protected function option_id_exist ($option_id)
+			{
+				return in_array($option_id, $this->_options) ;
+			}
+
+
+			protected function register_vote ($vote)
+			{
+
+				$i = 1 ;
+
+				foreach ($vote as $key => $value)
+				{
+					$vote[$i] = explode(',', $value) ;
+
+					$i++ ;
+				}
+
+				$this->_votes[] = $vote ;
+
+			}
 
 
 
--- test/test.php
+++ test/test.php
@@ -1,11 +1,20 @@
 <?php
 
+ini_set('xdebug.var_display_max_depth', -1);
+ini_set('xdebug.var_display_max_children', -1);
+ini_set('xdebug.var_display_max_data', -1);
+
 // UTF 8
 header('Content-Type: text/html; charset=utf-8') ;
 
 require_once '../Condorcet.class.php' ;
 
 $condorcet = new Condorcet () ;
+
+
+/////////////// REGISTER OPTIONS
+
+echo '<h2>Check Register Options</h2>';
 
 echo $condorcet->add_option('coucou').'<br>' ;
 echo $condorcet->add_option('kikoo').'<br>';
@@ -18,8 +27,27 @@ echo $condorcet->add_option().'<br>';
 echo $condorcet->add_option('B').'<br>';
 echo $condorcet->add_option(2).'<br>';
 
-echo '<br><br><hr><br><br>' ;
+echo '<br><br>' ;
 
 var_dump($condorcet);
 
+echo '<br><br><hr><br>';
+
+/////////////// REGISTER VOTES
+
+echo '<h2>Check Register Options</h2>';
+
+$vote_1[1] = 'A';
+$vote_1[2] = 'B,2,C';
+$vote_1[3] = 'coucou';
+
+
+echo $condorcet->add_vote($vote_1).'<br>' ;
+
+
+echo '<br><br>' ;
+
+var_dump($condorcet);
+
+echo '<br><br><hr><br>';
 
