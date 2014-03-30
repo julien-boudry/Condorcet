b7181dd84e63b6ca6eeac358cc70161d7daba24f
Julien Boudry <julien.boudry@supinternet.fr>
2014-03-30 22:48:00 +0200

    0.4 DEV

--- Condorcet.class.php
+++ Condorcet.class.php
@@ -2,7 +2,7 @@
 /*
 	Condorcet PHP Class, with Schulze Methods and others !
 
-	Version : 0.3 (maybe not ready for production, please test)
+	Version : 0.4
 
 	By Julien Boudry - MIT LICENSE (Please read LICENSE.txt)
 	https://github.com/julien-boudry/Condorcet_Schulze-PHP_Class 
