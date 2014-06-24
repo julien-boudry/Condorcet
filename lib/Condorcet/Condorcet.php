bfaa71d17712873242ba6cbb305fed899e8b4202
Julien Boudry <julien.boudry@supinternet.fr>
2014-06-24 23:57:03 +0200

    New daughter  for Exeption

--- lib/Condorcet/Condorcet.php
+++ lib/Condorcet/Condorcet.php
@@ -1399,3 +1399,43 @@ interface Condorcet_Algo
 	public function getResult($options);
 	public function getStats();
 }
+
+// Custom Exeption
+class CondorcetException extends \Exception
+{
+	public function __construct($code = 0, $infos = '')
+	{
+		$message = $this->correspondence($code);
+
+		parent::__construct($message, $code);
+	}
+
+
+	protected function correspondence ($code)
+	{
+		$error[1] = 'Bad candidate format';
+		$error[2] = 'The voting process has already started';
+		$error[3] = 'This candidate ID is already registered';
+		$error[4] = 'This candidate ID do not exist';
+		$error[5] = 'Bad vote format';
+		$error[6] = 'You need to specify votes before results';
+		$error[7] = 'Your Candidate ID is too long > ' . namespace\Condorcet::MAX_LENGTH_CANDIDATE_ID;
+		$error[8] = 'This method do not exist';
+		$error[9] = 'The algo class you want has not been defined';
+		$error[10] = 'The algo class you want is not correct';
+		$error[11] = 'You try to unserialize an object version older than your actual Class version. This is a problematic thing';
+		$error[12] = 'You have exceeded the number of votes allowed for this method.';
+		$error[13] = 'Formatting error: You do not multiply by a number!';
+		$error[14] = 'parseVote() must take a string (raw or path) as argument';
+		$error[15] = 'Input must be valid Json format';
+
+		if ( array_key_exists($code, $error) )
+		{
+			return $error[$code];
+		}
+		else
+		{
+			return (!is_null($infos)) ? $infos : 'Mysterious Error' ;
+		}
+	}
+}
