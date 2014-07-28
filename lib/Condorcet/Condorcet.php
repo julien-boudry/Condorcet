efc0e036a0fc5b457b11563d00dd83f683be086e
Julien Boudry <julien.boudry@supinternet.fr>
2014-07-29 01:12:14 +0200

    Version 0.13.2

--- lib/Condorcet/Condorcet.php
+++ lib/Condorcet/Condorcet.php
@@ -2,7 +2,7 @@
 /*
 	Condorcet PHP Class, with Schulze Methods and others !
 
-	Version : 0.13.1
+	Version : 0.13.2
 
 	By Julien Boudry - MIT LICENSE (Please read LICENSE.txt)
 	https://github.com/julien-boudry/Condorcet_Schulze-PHP_Class
@@ -28,7 +28,7 @@ class Condorcet
 /////////// CLASS ///////////
 
 
-	const VERSION = '0.13.1' ;
+	const VERSION = '0.13.2' ;
 	const ENV = 'STABLE' ;
 	const MAX_LENGTH_CANDIDATE_ID = 30 ; // Max length for candidate identifiant string
 
