bbd1d02df3f14556fa36ba23aa7230a370f1948e
Julien Boudry <julien.boudry@supinternet.fr>
2015-03-17 18:55:49 +0100

    Create new DEV ENV

--- lib/Condorcet/Condorcet.php
+++ lib/Condorcet/Condorcet.php
@@ -30,7 +30,7 @@ class Condorcet
 
 	const VERSION = '0.90' ;
 
-	const ENV = 'STABLE' ;
+	const ENV = 'DEV' ;
 	const MAX_LENGTH_CANDIDATE_ID = 30 ; // Max length for candidate identifiant string
 
 	protected static $_classMethod	= null ;
