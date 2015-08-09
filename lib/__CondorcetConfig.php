a282e1907ad38e131ab35b11a6cf72c36904f148
Julien Boudry <julien.boudry@supinternet.fr>
2015-08-09 19:54:48 +0200

    Rename Condorcet::addAlgos -> Condorcet::addMethod  3/3

--- lib/__CondorcetConfig.php
+++ lib/__CondorcetConfig.php
@@ -10,15 +10,15 @@
 namespace Condorcet;
 
 // Registering native Condorcet Methods implementation
-namespace\Condorcet::addAlgos(__NAMESPACE__.'\\Algo\\Methods\\Copeland');
-namespace\Condorcet::addAlgos(__NAMESPACE__.'\\Algo\\Methods\\KemenyYoung');
-namespace\Condorcet::addAlgos(__NAMESPACE__.'\\Algo\\Methods\\MinimaxWinning');
-namespace\Condorcet::addAlgos(__NAMESPACE__.'\\Algo\\Methods\\MinimaxMargin');
-namespace\Condorcet::addAlgos(__NAMESPACE__.'\\Algo\\Methods\\MinimaxOpposition');
-namespace\Condorcet::addAlgos(__NAMESPACE__.'\\Algo\\Methods\\RankedPairs');
-namespace\Condorcet::addAlgos(__NAMESPACE__.'\\Algo\\Methods\\SchulzeWinning');
-namespace\Condorcet::addAlgos(__NAMESPACE__.'\\Algo\\Methods\\SchulzeMargin');
-namespace\Condorcet::addAlgos(__NAMESPACE__.'\\Algo\\Methods\\SchulzeRatio');
+namespace\Condorcet::addMethod(__NAMESPACE__.'\\Algo\\Methods\\Copeland');
+namespace\Condorcet::addMethod(__NAMESPACE__.'\\Algo\\Methods\\KemenyYoung');
+namespace\Condorcet::addMethod(__NAMESPACE__.'\\Algo\\Methods\\MinimaxWinning');
+namespace\Condorcet::addMethod(__NAMESPACE__.'\\Algo\\Methods\\MinimaxMargin');
+namespace\Condorcet::addMethod(__NAMESPACE__.'\\Algo\\Methods\\MinimaxOpposition');
+namespace\Condorcet::addMethod(__NAMESPACE__.'\\Algo\\Methods\\RankedPairs');
+namespace\Condorcet::addMethod(__NAMESPACE__.'\\Algo\\Methods\\SchulzeWinning');
+namespace\Condorcet::addMethod(__NAMESPACE__.'\\Algo\\Methods\\SchulzeMargin');
+namespace\Condorcet::addMethod(__NAMESPACE__.'\\Algo\\Methods\\SchulzeRatio');
 
 // Set the default Condorcet Class algorithm
 namespace\Condorcet::setDefaultMethod('Schulze');
