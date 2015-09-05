351383aac3a61986802088e5231de22536859887
Boudry Julien <julien.boudry@gmail.com>
2015-09-05 20:24:21 +0200

    Prepare v0.97

--- lib/CondorcetException.php
+++ lib/CondorcetException.php
@@ -2,7 +2,7 @@
 /*
     Condorcet PHP Class, with Schulze Methods and others !
 
-    Version: 0.96
+    Version: 0.97
 
     By Julien Boudry - MIT LICENSE (Please read LICENSE.txt)
     https://github.com/julien-boudry/Condorcet
