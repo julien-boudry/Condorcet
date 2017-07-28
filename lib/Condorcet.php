c5e79955737811ef23c5af5fa20c9688f3da61b2
Julien Boudry <julien.boudry@gmail.com>
2017-07-28 16:02:06 +0200

    Version 1.2.1

--- lib/Condorcet.php
+++ lib/Condorcet.php
@@ -31,7 +31,7 @@ abstract class Condorcet
 {
 
 /////////// CONSTANTS ///////////
-        public const VERSION = '1.2.0';
+        public const VERSION = '1.2.1';
 
         public const ENV = 'DEV';
 
