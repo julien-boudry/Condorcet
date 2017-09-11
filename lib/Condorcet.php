0d17fb57dfb56f6284613748a17595793c638bb3
Julien Boudry <julien.boudry@gmail.com>
2017-09-11 23:47:47 +0200

    Fix commit 770a0091b2

--- lib/Condorcet.php
+++ lib/Condorcet.php
@@ -174,7 +174,7 @@ abstract class Condorcet
                 $input[$key] = self::format($line,false,$convertObject);
             endforeach;
 
-            if (count($input) === 1 && is_array(reset($input)) && count(reset($input)) === 1):
+            if (count($input) === 1 && (!is_array(reset($input)) || count(reset($input)) === 1)):
                 $r = reset($input);
             else:
                 $r = $input;
