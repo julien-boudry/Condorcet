afe2723687a6cc8d2e0b71bb94ba78bd45b2822d
Julien Boudry <julien.boudry@gmail.com>
2017-12-05 08:59:06 +0100

    New Result::getVotesListAsString method

---
+++ Tests/lib/ResultTest.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+namespace Condorcet;
+
+use Condorcet\Election;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Condorcet\Result
+ */
+class ResultTest extends TestCase
+{
+    /**
+     * @var election1
+     */
+    private $election1;
+
+    public function setUp()
+    {
+        $this->election1 = new Election;
+    }
+
+    public function testGetResultAsString ()
+    {
+        $this->election1->addCandidate('B');
+        $this->election1->addCandidate('A');
+        $this->election1->addCandidate('C');
+
+        $this->election1->parseVotes('
+            B > A > C * 7
+            A > B > C * 7
+            C > A > B * 2
+            C > B > A * 2
+        ');
+
+
+        self::assertSame(
+            'A = B > C',
+            $this->election1->getResult('Ranked Pairs')->getResultAsString()
+        );
+    }
+
+}
