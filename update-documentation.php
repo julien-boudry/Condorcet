2bc8637e988a37712c98e2bdad8ff006ef28b35b
Julien Boudry <julien.boudry@gmail.com>
2018-01-25 02:06:34 +0100

    Fix update documentation (need more tests)

--- update-documentation.php
+++ update-documentation.php
@@ -1,3 +1,5 @@
 <?php
-var_dump('vendor/bin/condorcet-doc "'.__DIR__.'/Documentation/"');
-    shell_exec('vendor/bin/condorcet-doc "'.__DIR__.'/Documentation/"');
+    $cmd = 'vendor/bin/condorcet-doc "'.__DIR__.'/Documentation"';
+    $cmd = str_replace('/', DIRECTORY_SEPARATOR, $cmd);
+
+    shell_exec($cmd);
