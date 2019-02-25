7343f7a19c433c025f73b6afe1993d78b4124ade
Julien Boudry <julien.boudry@gmail.com>
2019-02-25 18:47:03 +0100

    Add PhpBench with a very first benchmark

---
+++ Benchmarks/BasicUsageBench.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace CondorcetPHP\Condorcet\Benchmarks;
+
+use CondorcetPHP\Condorcet\Condorcet;
+use CondorcetPHP\Condorcet\Election;
+use CondorcetPHP\Condorcet\Candidate;
+use CondorcetPHP\Condorcet\CondorcetUtil;
+use CondorcetPHP\Condorcet\Vote;
+
+
+class BasicUsageBench
+{
+    /**
+     * @Iterations(4)
+     * @Warmup(1)
+     * @Revs(4)
+     */
+    public function benchSimpleManyVotes () : void
+    {
+       $election = new Election;
+       $election->allowVoteWeight(true);
+
+       $election->parseCandidates('A;B;C;D;E;F');
+
+       $election->parseVotes('
+       		Ultimate Question of Life || A>B>C ^42 * 42
+          C=A>B ^2 * 200
+          B>C
+          E > B > C > A ^80 *50
+          F > B > G > H > A* 250
+          D = B = E > F ^6 * 48
+       ');
+
+       $election->getWinner();
+       $election->getLoser();
+
+       foreach (Condorcet::getAuthMethods() as $method) :
+         $election->getResult($method);
+       endforeach;
+
+       $election->setImplicitRanking(false);
+
+       foreach (Condorcet::getAuthMethods() as $method) :
+         $election->getResult($method);
+       endforeach;
+
+       $election->allowVoteWeight(false);
+
+       foreach (Condorcet::getAuthMethods() as $method) :
+         $election->getResult($method);
+       endforeach;
+
+       $election->parseVotes('
+          Ultimate Question of Life || C>B>A ^42 * 42
+          C=A=B ^2 * 200
+          B>C
+          A > C >E ^80 *50
+          G > B > G > H > F* 250
+          C = B = E > A ^6 * 48
+       ');
+
+       foreach (Condorcet::getAuthMethods() as $method) :
+         $election->getResult($method);
+       endforeach;
+    }
+
+}
