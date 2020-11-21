393c1e083e6fc4e2efaf7ae4305e3ff6a962b3dd
Julien Boudry <julien.boudry@gmail.com>
2020-11-21 22:38:41 +0100

    Some fix about doc. migration tools

--- Dev/migrate_yamlDoc_to_attributes.php
+++ Dev/migrate_yamlDoc_to_attributes.php
@@ -22,9 +22,6 @@ unset($doc[1]);
 $index  = [];
 
 foreach ($doc as &$entry) :
-  if (isset($entry['publish']) && $entry['publish'] !== true) :
-    continue;
-  endif;
 
   if (!is_array($entry['class'])) :
     $entry['class'] = [$entry['class']];
@@ -43,7 +40,11 @@ $patternReplaceLastNewLine = '/(.*)\n$/';
 
 foreach ($index as $ClassName => $ClassData) :
 
-    $path_to_file = [__DIR__.'/../lib/'.str_replace('/','\\',$ClassName).'.php'];
+    $path_to_file = [
+                        __DIR__.'/../lib/'.str_replace('\\','/',$ClassName).'.php',
+                        __DIR__.'/../lib/CondorcetVersion.php',
+                        __DIR__.'/../lib/Linkable.php'
+                    ];
 
     if ($ClassName === 'Election') :
         $path_to_file[] = __DIR__.'/../lib/ElectionProcess/CandidatesProcess.php';
@@ -51,7 +52,12 @@ foreach ($index as $ClassName => $ClassData) :
         $path_to_file[] = __DIR__.'/../lib/ElectionProcess/ResultsProcess.php';
     endif;
 
+
+
+    // var_dump($path_to_file);
+
     foreach ($path_to_file as $oneFile) :
+        $file_contents = file_get_contents($oneFile);
 
         foreach($ClassData as $MethodName => $MethodData) :
             var_dump($ClassName.'->'.$MethodName);
@@ -59,7 +65,7 @@ foreach ($index as $ClassName => $ClassData) :
             $description = $MethodData['description'];
             $description = str_replace("\$","\\\\$", $description);
             $description = preg_replace($patternReplaceLastNewLine, '$1', $description);
-            $description = str_replace(["\n    ","\n"],"\\n ",$description);
+            $description = str_replace(["\n    ","\n"],"\\n",$description);
             $description = str_replace('"','\"',$description);
 
             $attributes =  '$3#[Description("'.$description.'")]$2';
@@ -67,7 +73,7 @@ foreach ($index as $ClassName => $ClassData) :
             if (isset($MethodData['return'])) :
                 $returnV = $MethodData['return'];
                 $returnV = preg_replace($patternReplaceLastNewLine, '$1', $returnV);
-                $returnV = str_replace(["\n    ","\n"],"\\n ",$returnV);
+                $returnV = str_replace(["\n    ","\n"],"\\n",$returnV);
                 $returnV = str_replace('"','\"',$returnV);
 
                 $attributes .= '$3#[FunctionReturn("'.$returnV.'")]$2';
@@ -77,17 +83,17 @@ foreach ($index as $ClassName => $ClassData) :
             if (isset($MethodData['examples'])) :
                 $examples = $MethodData['examples'];
                 $examples = preg_replace($patternReplaceLastNewLine, '$1', $examples);
-                $examples = str_replace(["\n    ","\n"],"\\n ",$examples);
+                $examples = str_replace(["\n    ","\n"],"\\n",$examples);
                 $examples = str_replace('"','\"',$examples);
 
                 $arg = '';
                 $i = 1;
-                foreach($examples as $oneExample) :
+                foreach($examples as $oneExampleKey => $oneExample) :
                   if ($i++ > 1) :
                     $arg .= ', ';
                   endif;
 
-                  $arg .= '"'.$oneExample.'"';
+                  $arg .= '"'.$oneExampleKey.'||'.$oneExample.'"';
                 endforeach;
 
                 $attributes .= '$3#[Examples('.$arg.')]$2';
@@ -95,8 +101,6 @@ foreach ($index as $ClassName => $ClassData) :
 
             if (isset($MethodData['related'])) :
               $related = $MethodData['related'];
-              $related = preg_replace($patternReplaceLastNewLine, '$1', $related);
-              $related = str_replace(["\n    ","\n"],"\\n ",$related);
               $related = str_replace('"','\"',$related);
 
               $arg = '';
@@ -113,16 +117,16 @@ foreach ($index as $ClassName => $ClassData) :
           endif;
 
 
-            $pattern = '/(#\[PublicAPI\])(\n)(.*)(public function '.$MethodName.')/';
+            $pattern = '/(#\[PublicAPI\])(\n)(.*)(public.*function '.$MethodName.' *\()/';
             $replacement = '$1$2'.$attributes.'$3$4';
             
-            $file_contents = file_get_contents($oneFile);
             $file_contents = preg_replace(
                     $pattern
                 ,   $replacement
                 ,   $file_contents);
-            file_put_contents($oneFile,$file_contents);
         endforeach;
+
+        file_put_contents($oneFile,$file_contents);
 
     endforeach;
 
