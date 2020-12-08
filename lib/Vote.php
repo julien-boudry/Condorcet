39ce73152555ca13824ca49aef795e8379df2626
Julien Boudry <julien.boudry@gmail.com>
2020-12-08 20:47:47 +0100

    Bugfix: Input new vote from string can create duplicate candidate on the same ranking, computing invalid pairwise. Input from object was not impacted. It should fix issue #32.

--- lib/Vote.php
+++ lib/Vote.php
@@ -408,7 +408,7 @@ class Vote implements \Iterator, \Stringable
                 // Check Duplicate
 
                     // Check objet reference AND check candidates name
-                    if (!\in_array($Candidate, $list_candidate)) :
+                    if (!\in_array($Candidate->getName(), $list_candidate)) :
                         $list_candidate[] = $Candidate;
                     else :
                         throw new CondorcetException(5);
