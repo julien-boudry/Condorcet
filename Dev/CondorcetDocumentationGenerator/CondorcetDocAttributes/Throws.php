2b1f52b23764756755359ace1cc598a64dd33e46
Dr. Tobias Quathamer <toddy@debian.org>
2021-09-06 19:24:35 +0200

    Add new PHP attribute for 'Throws'

---
+++ Dev/CondorcetDocumentationGenerator/CondorcetDocAttributes/Throws.php
@@ -0,0 +1,24 @@
+<?php
+/*
+    Condorcet PHP - Election manager and results calculator.
+    Designed for the Condorcet method. Integrating a large number of algorithms extending Condorcet. Expandable for all types of voting systems.
+
+    By Julien Boudry and contributors - MIT LICENSE (Please read LICENSE.txt)
+    https://github.com/julien-boudry/Condorcet
+*/
+declare(strict_types=1);
+
+namespace CondorcetPHP\Condorcet\Dev\CondorcetDocumentationGenerator\CondorcetDocAttributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class Throws
+{
+    public array $exceptionList;
+
+    public function __construct(string ...$exceptionList)
+    {
+        $this->exceptionList = $exceptionList;
+    }
+}
