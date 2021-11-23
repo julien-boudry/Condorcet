3589472cf6e3994e588c712bce44e96efcef427c
Julien Boudry <julien.boudry@gmail.com>
2021-11-23 18:26:32 +0100

    Prepare 3.2

--- lib/Condorcet.php
+++ lib/Condorcet.php
@@ -44,7 +44,7 @@ abstract class Condorcet
 
 /////////// CONSTANTS ///////////
     #[PublicAPI]
-    final public const VERSION = '3.1.0';
+    final public const VERSION = '3.2.0';
 
     #[PublicAPI]
     final public const CONDORCET_BASIC_CLASS = Algo\Methods\CondorcetBasic::class;
