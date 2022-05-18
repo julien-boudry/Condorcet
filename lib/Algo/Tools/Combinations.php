a614061754eef476d97d2e05d3b3066666f40627
Julien Boudry <julien.boudry@gmail.com>
2022-05-18 21:17:35 +0200

    Use SplFixedArray for Combinations

--- lib/Algo/Tools/Combinations.php
+++ lib/Algo/Tools/Combinations.php
@@ -10,15 +10,33 @@ declare(strict_types=1);
 
 namespace CondorcetPHP\Condorcet\Algo\Tools;
 
+use SplFixedArray;
+
 class Combinations
 {
-    public static function compute (array $values, int $length, array $append_before = []): array
+    public static function compute (array $values, int $length, array $append_before = []): SplFixedArray
     {
         $count = \count($values);
         $size = 2 ** $count;
         $keys = \array_keys($values);
-        $return = [];
 
+        // Number of combinations
+        $a = 1;
+        for ($i = $count ; $i > ($count - $length) ; $i--) :
+            $a = $a * $i;
+        endfor;
+
+        $b = 1;
+        for ($i = $length ; $i > 0 ; $i--) :
+            $b = $b * $i;
+        endfor;
+
+        $combinations_count = (int) $a / $b;
+
+        // Get the combinations
+        $return = new SplFixedArray($combinations_count);
+
+        $arrKey = 0;
         for ($i = 0; $i < $size; $i++) :
             $b = \sprintf("%0" . $count . "b", $i);
             $out = [];
@@ -30,7 +48,7 @@ class Combinations
             endfor;
 
             if (count($out) === $length) :
-                 $return[] = \array_merge($append_before, $out);
+                 $return[$arrKey++] = \array_merge($append_before, $out);
             endif;
         endfor;
 
