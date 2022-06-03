2b9a4ee70e959251fd5ecf4d4d4b76876e2877e2
Julien Boudry <julien.boudry@gmail.com>
2022-06-03 15:39:33 +0200

    Add Combinations tests

--- Tests/src/Algo/Tools/CombinationsTest.php
+++ Tests/src/Algo/Tools/CombinationsTest.php
@@ -13,6 +13,17 @@ class CombinationsTest extends TestCase
     public function testCountPossibleCombinationsResult (): void
     {
         self::assertSame(2_598_960, Combinations::getNumberOfCombinations(52,5)); // Card Game
+
+        self::assertSame(5_278_511_093_147_205_632, Combinations::getNumberOfCombinations(78,24)); // Tarot Card Game - 4 players
+        self::assertSame(212_566_476_905_162_400, Combinations::getNumberOfCombinations(78,18)); // Tarot Card Game - 4 players
+        self::assertSame(4_367_914_309_753_281, Combinations::getNumberOfCombinations(78,15)); // Tarot Card Game - 5 players
+
+        // Usual permutation for CPO STV 11 candidates and 3 seats left
+        self::assertSame(13_530,    Combinations::getNumberOfCombinations(  count: Combinations::getNumberOfCombinations(
+                                    count: 11,
+                                    length: 3),
+                                length: 2)
+        );
     }
 
     public function testCountPossibleCombinationsBadParameters1 (): void
