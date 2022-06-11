7800a2d378ba69e7043690e958a20584f190cb03
Julien Boudry <julien.boudry@gmail.com>
2022-06-11 11:58:49 +0200

    Backport Methods Benchmarks from 4.0

---
+++ Benchmarks/MethodsNonProportionalBench.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+
+namespace CondorcetPHP\Condorcet\Benchmarks;
+
+use CondorcetPHP\Condorcet\Algo\Methods\KemenyYoung\KemenyYoung;
+use CondorcetPHP\Condorcet\Algo\Methods\RankedPairs\RankedPairs_Core;
+use CondorcetPHP\Condorcet\Algo\StatsVerbosity;
+use CondorcetPHP\Condorcet\Condorcet;
+use CondorcetPHP\Condorcet\Election;
+use CondorcetPHP\Condorcet\Throwable\CandidatesMaxNumberReachedException;
+use CondorcetPHP\Condorcet\Throwable\MethodLimitReachedException;
+use PhpBench\Attributes as Bench;
+ini_set('memory_limit','51200M');
+
+class MethodsNonProportionalBench
+{
+    public bool $IS_A_PROPORTIONAL_BENCH = false;
+
+    public array $numberOfCandidates = [3,5,6,7,8,9,10,11,20,30,40,50,60,70,80,90,100];
+
+    protected Election $election;
+
+    public function __construct ()
+    {
+        RankedPairs_Core::$MaxCandidates = null;
+        KemenyYoung::$MaxCandidates = 9;
+    }
+
+
+    protected function buildElection (int $numberOfCandidates, int $numberOfVotes): void
+    {
+        $this->election = $election = new Election;
+        $this->election->setNumberOfSeats(max(1, (int) ($numberOfCandidates / 3)));
+        // $this->election->setStatsVerbosity(StatsVerbosity::STD);
+
+        $candidates = [];
+
+        for ($i=0 ; $i < $numberOfCandidates ; $i++) :
+            $candidates[] = $election->addCandidate();
+        endfor;
+
+        for ($i = 0 ; $i < $numberOfVotes ; $i++) :
+            $oneVote = $candidates;
+            shuffle($oneVote);
+
+            $election->addVote($oneVote);
+        endfor;
+    }
+
+    public function provideMethods (): \Generator
+    {
+        foreach (Condorcet::getAuthMethods() as $method) :
+            $class = Condorcet::getMethodClass($method);
+
+            if ($class::IS_PROPORTIONAL === $this->IS_A_PROPORTIONAL_BENCH) :
+                yield $method => ['method' => $method];
+            endif;
+        endforeach;
+    }
+
+    public function provideNumberOfCandidates (): \Generator
+    {
+        foreach ($this->numberOfCandidates as $n) :
+            yield $n => ['numberOfCandidates' => $n];
+        endforeach;
+    }
+
+    public function setUp (array $params): void
+    {
+        $this->buildElection($params['numberOfCandidates'], 1_000);
+    }
+
+    #[Bench\OutputTimeUnit('seconds')]
+    #[Bench\ParamProviders(['provideMethods', 'provideNumberOfCandidates'])]
+    #[Bench\BeforeMethods('setUp')]
+    #[Bench\Warmup(1)]
+    #[Bench\Iterations(3)]
+    #[Bench\Revs(1)]
+    public function benchByCandidates (array $params): void
+    {
+        try {
+            $result = $this->election->getResult($params['method']);
+        } catch (CandidatesMaxNumberReachedException $e) {}
+
+        $this->election->cleanupCalculator();
+    }
+}
---
+++ Benchmarks/MethodsProportionalBench.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace CondorcetPHP\Condorcet\Benchmarks;
+
+use CondorcetPHP\Condorcet\Algo\Methods\STV\CPO_STV;
+
+ini_set('memory_limit','51200M');
+
+class MethodsProportionalBench extends MethodsNonProportionalBench
+{
+    public bool $IS_A_PROPORTIONAL_BENCH = true;
+
+    public array $numberOfCandidates = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90,100];
+
+    public function __construct ()
+    {
+        // CPO_STV::$MaxOutcomeComparisons = 200_000;
+    }
+}
