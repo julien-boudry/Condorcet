b8be7bfad13ba6dd341739929b78d5284497139b
Julien Boudry <julien.boudry@gmail.com>
2023-03-23 07:04:59 +0100

    Console debug verbosity display the used color mode

--- src/Console/Commands/ElectionCommand.php
+++ src/Console/Commands/ElectionCommand.php
@@ -678,7 +678,8 @@ class ElectionCommand extends Command
 
         $this->io->newLine();
         $this->io->writeln('<comment>'.str_repeat('-', 23).'</comment>', OutputInterface::VERBOSITY_VERBOSE);
-        $this->io->writeln("<comment>Execution Time: {$executionTime}s</comment>", OutputInterface::VERBOSITY_VERBOSE);
+        $this->io->writeln("<comment>Execution Time: <condor2>{$executionTime}s</condor2></comment>", OutputInterface::VERBOSITY_VERBOSE);
+        $this->io->writeln("<comment>Ansi terminal color mode: <condor2>".Terminal::getColorMode()->name."</condor2></comment>", OutputInterface::VERBOSITY_DEBUG);
         $this->io->newLine();
     }
 
