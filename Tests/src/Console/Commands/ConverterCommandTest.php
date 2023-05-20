76c17ef811eda3e1b10614b655d97ada42eff83d
Julien Boudry <julien.boudry@gmail.com>
2023-05-20 11:10:40 +0200

    .temp extension for tests fileq generated

--- Tests/src/Console/Commands/ConverterCommandTest.php
+++ Tests/src/Console/Commands/ConverterCommandTest.php
@@ -16,7 +16,7 @@ class ConverterCommandTest extends TestCase
     public const DEBIAN_INPUT_FILE = __DIR__.'/../../Tools/Converters/DebianData/leader2020_tally.txt';
     public const DAVIDHILL_INPUT_FILE = __DIR__.'/../../Tools/Converters/TidemanData/A77.HIL';
 
-    public const OUTPUT_FILE = __DIR__.'/files/out.txt';
+    public const OUTPUT_FILE = __DIR__.'/files/out.temp';
 
     private readonly CommandTester $converterCommand;
 
