95a6e4b8d74cfc634ef9aa55aac2f7c5971bdddc
Julien Boudry <julien.boudry@gmail.com>
2023-05-26 18:22:02 +0200

    New Votes candidates Ranked Stats internal tools for methods

---
+++ Tests/src/Algo/Tools/VotesCandidatesRankedStatsImmutableTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CondorcetPHP\Condorcet\Tests\Algo\Tools;
+
+use CondorcetPHP\Condorcet\Algo\Tools\{Combinations, VotesCandidatesRankedStatsImmutable};
+use CondorcetPHP\Condorcet\Election;
+use PHPUnit\Framework\TestCase;
+
+class VotesCandidatesRankedStatsImmutableTest extends TestCase
+{
+    public function testGetVotesCountWithCandidatesAllCombination(): void
+    {
+        $election = new Election;
+
+        $election->addCandidate('A'); // 0
+        $election->addCandidate('B'); // 1
+        $election->addCandidate('C'); // 2
+        $election->addCandidate('D'); // 3
+        $election->addCandidate('E'); // 4
+        $election->addCandidate('F'); // 5
+        $election->addCandidate('G'); // 6
+        $election->addCandidate('H'); // 7
+        $election->addCandidate('I'); // 8
+        $election->addCandidate('J'); // 9
+
+        $election->parseVotes('A>B>C=D>E ^42');
+
+        // Test with 2 and implicit
+        $votesStats = new VotesCandidatesRankedStatsImmutable(2, $election);
+        self::assertCount(Combinations::getPossibleCountOfCombinations($election->countCandidates(), 2), $votesStats);
+
+        self::assertSame(1, $votesStats->sumWeightIfVotesIncludeCandidates([0, 1]));
+        self::assertSame(1, $votesStats->sumWeightIfVotesIncludeCandidates([1, 0]));
+
+        self::assertSame(1, $votesStats->sumWeightIfVotesIncludeCandidates([2, 3]));
+        self::assertSame(1, $votesStats->sumWeightIfVotesIncludeCandidates([4, 0]));
+        self::assertSame(1, $votesStats->sumWeightIfVotesIncludeCandidates([1, 8]));
+        self::assertSame(1, $votesStats->sumWeightIfVotesIncludeCandidates([8, 9]));
+
+        // Test with 2 and explicit
+        $election->setImplicitRanking(false);
+
+        $votesStats = $votesStats = new VotesCandidatesRankedStatsImmutable(2, $election);
+        self::assertCount(Combinations::getPossibleCountOfCombinations($election->countCandidates(), 2), $votesStats);
+
+        self::assertSame(1, $votesStats->sumWeightIfVotesIncludeCandidates([0, 1]));
+        self::assertSame(1, $votesStats->sumWeightIfVotesIncludeCandidates([3, 2]));
+        self::assertSame(1, $votesStats->sumWeightIfVotesIncludeCandidates([0, 4]));
+        self::assertSame(0, $votesStats->sumWeightIfVotesIncludeCandidates([1, 8]));
+        self::assertSame(0, $votesStats->sumWeightIfVotesIncludeCandidates([8, 9]));
+
+        // Test with 5 and explicit
+        $votesStats = $votesStats = new VotesCandidatesRankedStatsImmutable(5, $election);
+        self::assertCount(Combinations::getPossibleCountOfCombinations($election->countCandidates(), 5), $votesStats);
+
+        self::assertSame(1, $votesStats->sumWeightIfVotesIncludeCandidates([0, 1, 2, 3, 4]));
+        self::assertSame(0, $votesStats->sumWeightIfVotesIncludeCandidates([0, 1, 2, 3, 5]));
+        self::assertSame(0, $votesStats->sumWeightIfVotesIncludeCandidates([3, 8, 6, 9, 2]));
+        self::assertSame(0, $votesStats->sumWeightIfVotesIncludeCandidates([5, 6, 8, 7, 9]));
+
+        // With 2 and weight activated
+        $election->allowsVoteWeight(true);
+
+        $votesStats = $votesStats = new VotesCandidatesRankedStatsImmutable(2, $election);
+        self::assertCount(Combinations::getPossibleCountOfCombinations($election->countCandidates(), 2), $votesStats);
+
+        self::assertSame(42, $votesStats->sumWeightIfVotesIncludeCandidates([0, 1]));
+        self::assertSame(42, $votesStats->sumWeightIfVotesIncludeCandidates([3, 2]));
+        self::assertSame(42, $votesStats->sumWeightIfVotesIncludeCandidates([0, 4]));
+        self::assertSame(0, $votesStats->sumWeightIfVotesIncludeCandidates([1, 8]));
+        self::assertSame(0, $votesStats->sumWeightIfVotesIncludeCandidates([8, 9]));
+
+        // Add a vote
+        $election->parseVotes('A>B');
+        $votesStats = $votesStats = new VotesCandidatesRankedStatsImmutable(2, $election);
+
+        self::assertSame(43, $votesStats->sumWeightIfVotesIncludeCandidates([0, 1]));
+        self::assertSame(42, $votesStats->sumWeightIfVotesIncludeCandidates([0, 2]));
+        self::assertSame(0, $votesStats->sumWeightIfVotesIncludeCandidates([1, 8]));
+    }
+}
---
+++ src/Algo/Tools/VotesCandidatesRankedStatsImmutable.php
@@ -0,0 +1,85 @@
+<?php
+/*
+    Condorcet PHP - Election manager and results calculator.
+    Designed for the Condorcet method. Integrating a large number of algorithms extending Condorcet. Expandable for all types of voting systems.
+
+    By Julien Boudry and contributors - MIT LICENSE (Please read LICENSE.txt)
+    https://github.com/julien-boudry/Condorcet
+*/
+
+declare(strict_types=1);
+
+/////////// TOOLS FOR MODULAR ALGORITHMS ///////////
+
+namespace CondorcetPHP\Condorcet\Algo\Tools;
+
+use CondorcetPHP\Condorcet\{Candidate, Election};
+use CondorcetPHP\Condorcet\Dev\CondorcetDocumentationGenerator\CondorcetDocAttributes\{Description, InternalModulesAPI};
+use CondorcetPHP\Condorcet\Throwable\Internal\CondorcetInternalException;
+use CondorcetPHP\Condorcet\Relations\HasElection;
+
+// Generic for Algorithms
+class VotesCandidatesRankedStatsImmutable implements \Countable
+{
+    use HasElection;
+
+    protected static function voteHasCandidates(array $voteCandidatesKey, array $combination): bool
+    {
+        foreach ($combination as $oneCombinationCandidateKey) {
+            if (!\in_array($oneCombinationCandidateKey, $voteCandidatesKey, true)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    protected static function getCombinationsScoreKey(array $oneCombination): string
+    {
+        sort($oneCombination, \SORT_NUMERIC);
+        return implode('_', $oneCombination);
+    }
+
+    ///
+
+    protected array $combinationsScore = [];
+
+    #[InternalModulesAPI]
+    #[Description('Build the object.')]
+    public function __construct(public readonly int $subsetSize, Election $election)
+    {
+        $this->setElection($election);
+
+        $candidatesList = $this->getElection()->getCandidatesList();
+        $candidatesKey = array_keys($candidatesList);
+        rsort($candidatesKey, \SORT_NUMERIC);
+
+        foreach (Combinations::computeGenerator($candidatesKey, $subsetSize) as $oneCombination) {
+            $this->combinationsScore[self::getCombinationsScoreKey($oneCombination)] = 0;
+        }
+
+        foreach ($this->getElection()->getVotesValidUnderConstraintGenerator() as $oneVote) {
+            $voteCandidates = $oneVote->getAllCandidates($this->getElection());
+            $voteCandidates = array_map(static fn (Candidate $c): ?int => $election->getCandidateKey($c), $voteCandidates);
+            $voteCandidates = array_filter($voteCandidates, static fn (?int $c) => $c !== null);
+
+            foreach (Combinations::computeGenerator($candidatesKey, $subsetSize) as $oneCombination) {
+                if (self::voteHasCandidates($voteCandidates, $oneCombination)) {
+                    $this->combinationsScore[self::getCombinationsScoreKey($oneCombination)] += $oneVote->getWeight($this->getElection());
+                }
+            }
+        }
+    }
+
+    // Implement Countable
+    public function count(): int
+    {
+        return \count($this->combinationsScore);
+    }
+
+    #[InternalModulesAPI]
+    public function sumWeightIfVotesIncludeCandidates(array $candidatesKeys): int
+    {
+        return $this->combinationsScore[self::getCombinationsScoreKey($candidatesKeys)] ?? throw new CondorcetInternalException('This combination does not exist in this stats object');
+    }
+}
