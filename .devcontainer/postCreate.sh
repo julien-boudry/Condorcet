6a0e1abde0af00edd86f811636ed8fce45a3d958
Julien Boudry <julien.boudry@gmail.com>
2023-07-11 16:57:30 +0200

    Dev Container - Activate JIT

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -7,6 +7,8 @@ sudo bash -c 'echo "zend_extension=opcache" >> /usr/local/etc/php/conf.d/custom.
 sudo bash -c 'echo "opcache.enable=1" >> /usr/local/etc/php/conf.d/custom.ini'
 sudo bash -c 'echo opcache.enable_cli=1 >> /usr/local/etc/php/conf.d/custom.ini'
 sudo bash -c 'echo opcache.jit=tracing >> /usr/local/etc/php/conf.d/custom.ini'
+sudo bash -c 'echo opcache.jit_buffer_size=512M >> /usr/local/etc/php/conf.d/custom.ini'
+
 
 #Xdebug
 sudo bash -c "sed -i 's/xdebug.start_with_request = yes/xdebug.start_with_request = no/' /usr/local/etc/php/conf.d/xdebug.ini"
