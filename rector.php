f2ba195d6ed5d0c6d44ded45895b11644727c8ed
Julien Boudry <julien.boudry@gmail.com>
2025-04-02 11:34:59 +0200

    SetupRector

---
+++ rector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/Benchmarks',
+        __DIR__ . '/Dev',
+        __DIR__ . '/Examples',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    // uncomment to reach your current PHP version
+    ->withPhpSets()
+    // ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    // ->withCodeQualityLevel(0)
+
+    ->withSkip([
+        ClassPropertyAssignToConstructorPromotionRector::class,
+        NullToStrictStringFuncCallArgRector::class,
+        RestoreDefaultNullToNullableTypePropertyRector::class,
+    ])
+;
