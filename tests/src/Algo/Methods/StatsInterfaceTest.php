2c809b9567048f5d67bcd94c485d7a9710b2a2bd
Julien Boudry <julien.boudry@gmail.com>
2025-04-07 23:34:45 +0200

    Add test for BaseMethodStats in StatsInterfaceTest, must be closed

---
+++ tests/src/Algo/Methods/StatsInterfaceTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use CondorcetPHP\Condorcet\Algo\Stats\BaseMethodStats;
+use CondorcetPHP\Condorcet\Condorcet;
+use CondorcetPHP\Condorcet\Election;
+
+test('BaseMethodStats Are closed', function (): void {
+    $election = new Election;
+
+    $election->parseCandidates('A,B,C');
+    $election->addVote('A > B > C');
+
+    foreach(Condorcet::getAuthMethods() as $method) {
+        $stats = $election->getResult($method)->stats;
+
+        if ($stats instanceof BaseMethodStats)
+        {
+            expect($stats->closed)->toBe(true);
+        }
+    }
+});
