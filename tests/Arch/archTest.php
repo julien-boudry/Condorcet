32277dc37948a53521ccc2624a272aa1ab8d5a65
Julien Boudry <julien.boudry@proton.me>
2025-11-29 12:11:30 +0100

    Linter

--- tests/Arch/archTest.php
+++ tests/Arch/archTest.php
@@ -6,8 +6,8 @@ use CondorcetPHP\Condorcet\Dev\CondorcetDocumentationGenerator\Generate;
 
 if (\PHP_OS_FAMILY !== 'Windows') { // Prevent Pest issues on Windows
     arch()->preset()->php()
-            ->ignoring('debug_backtrace') // Autorisation de debug_backtrace
-            ->ignoring(Generate::class)
+        ->ignoring('debug_backtrace') // Autorisation de debug_backtrace
+        ->ignoring(Generate::class)
     ;
 }
 
